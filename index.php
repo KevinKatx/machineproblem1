844021933d008620a221d5b72fc1853783d9aeef
KevinKatx <123385970+KevinKatx@users.noreply.github.com>
2024-12-08 15:27:53 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,149 @@
+<?php
+$movies = [
+    ["title" => "Gladiator 2", "image" => "media/movies/gladiator2.jpg", "trailer" => "media/trailers/gladiator2.mp4", "description" => "Years after witnessing the death of Maximus at the hands of his uncle, Lucius must enter the Colosseum after the powerful emperors of Rome conquer his home. With rage in his heart and the future of the empire at stake, he looks to the past to find the strength and honor needed to return the glory of Rome to its people."],
+    ["title" => "Interstellar", "image" => "media/movies/interstellar.jpg", "trailer" => "media/trailers/interstellar.mp4", "description" => "In Earth's future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth's population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind's new home."],
+    ["title" => "Johnwick", "image" => "media/movies/johnwick.jpg", "trailer" => "media/trailers/johnwick.mp4", "description" => "John Wick is a 2014 American action thriller film directed by Chad Stahelski and written by Derek Kolstad. Keanu Reeves stars as John Wick, a legendary hitman who comes out of retirement to seek revenge against the men who killed his dog, a final gift from his recently deceased wife."],
+    ["title" => "Kingsman: The Golden Circle", "image" => "media/movies/kingsmangc.jpg", "trailer" => "media/trailers/kingsmangc.mp4", "description" => "With their headquarters destroyed and the world held hostage, members of Kingsman find new allies when they discover a spy organization in the United States known as Statesman. In an adventure that tests their strength and wits, the elite secret agents from both sides of the pond band together to battle a ruthless enemy and save the day, something that's becoming a bit of a habit for Eggsy."],
+    ["title" => "Man of Steel", "image" => "media/movies/manofsteel.jpg", "trailer" => "media/trailers/manofsteel.mp4", "description" => "With the imminent destruction of Krypton, their home planet, Jor-El (Russell Crowe) and his wife seek to preserve their race by sending their infant son to Earth. The child's spacecraft lands at the farm of Jonathan (Kevin Costner) and Martha (Diane Lane) Kent, who name him Clark and raise him as their own son. Though his extraordinary abilities have led to the adult Clark (Henry Cavill) living on the fringe of society, he finds he must become a hero to save those he loves from a dire threat."],
+    ["title" => "Moana 2", "image" => "media/movies/moana2.jpg", "trailer" => "media/trailers/moana2.mp4", "description" => "Moana journeys to the far seas of Oceania after receiving an unexpected call from her wayfinding ancestors."],
+    ["title" => "Spellbound", "image" => "media/movies/spellbound.jpg", "trailer" => "media/trailers/spellbound.mp4", "description" => "When a powerful spell turns her parents into giant monsters, a teenage princess must journey into the wild to reverse the curse before it's too late."],
+    ["title" => "Spiderman: No Way Home", "image" => "media/movies/spidermannwh.jpg", "trailer" => "media/trailers/spidermannwh.mp4", "description" => "Peter Parker's relaxing European vacation takes an unexpected turn when Nick Fury shows up in his hotel room to recruit him for a mission. The world is in danger as four massive elemental creatures -- each representing Earth, air, water and fire -- emerge from a hole torn in the universe. Parker soon finds himself donning the Spider-Man suit to help Fury and fellow superhero Mysterio stop the evil entities from wreaking havoc across the continent."],
+    ["title" => "White Chicks", "image" => "media/movies/whitechicks.jpg", "trailer" => "media/trailers/whitechicks.mp4", "description" => "Two FBI agent brothers, Marcus (Marlon Wayans) and Kevin Copeland (Shawn Wayans), accidentally foil a drug bust. As punishment, they are forced to escort a pair of socialites (Anne Dudek, Rochelle Aytes) to the Hamptons, where they're going to be used as bait for a kidnapper. But when the girls realize the FBI's plan, they refuse to go. Left without options, Marcus and Kevin decide to pose as the sisters, transforming themselves from African-American men into a pair of blonde, white women."],
+    ["title" => "Wicked", "image" => "media/movies/wicked.jpg", "trailer" => "media/trailers/wicked.mp4", "description" => "Misunderstood because of her green skin, a young woman named Elphaba forges an unlikely but profound friendship with Glinda, a student with an unflinching desire for popularity. Following an encounter with the Wizard of Oz, their relationship soon reaches a crossroad as their lives begin to take very different paths."]
+];
+
+$movies2= [
+    ["title" => "Dune: Part Two", "image" => "media/movies/movies2/dune2.png", "trailer" => "media/trailers/movies2/dune2.mp4", "description" => "Dune: Part Two will explore the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a warpath of revenge against the conspirators who destroyed his family."],
+    ["title" => "How to Train Your Dragon 2", "image" => "media/movies/movies2/httyd2.PNG", "trailer" => "media/trailers/movies2/httyd2.mp4", "description" => "Five years have passed since Hiccup and Toothless united the dragons and Vikings of Berk. Now, they spend their time charting the island's unmapped territories. During one of their adventures, the pair discover a secret cave that houses hundreds of wild dragons -- and a mysterious dragon rider."],
+    ["title" => "Kung Fu Panda", "image" => "media/movies/movies2/kfpanda.PNG", "trailer" => "media/trailers/movies2/kfpanda.mp4", "description" => "This movie follows the story of Po (voice of Jack Black), a Panda who works in his father's noodle shop, but dreams of becoming a Kung Fu champion. When Po learns that the village is hosting a tournament to nominate a Dragon Warrior, he makes his way to the Jade Palace to watch the festivities."],
+    ["title" => "Transformers One", "image" => "media/movies/movies2/tfone.jpg", "trailer" => "media/trailers/movies2/tfone.mp4", "description" => "TRANSFORMERS ONE is the untold origin story of Optimus Prime and Megatron, better known as sworn enemies, but once were friends bonded like brothers who changed the fate of Cybertron forever."],
+    ["title" => "The Wild Robot", "image" => "media/movies/movies2/wildrobot.jpg", "trailer" => "media/trailers/movies2/wildrobot.mp4", "description" => "The epic adventure follows the journey of a robot—ROZZUM unit 7134, “Roz” for short — that is shipwrecked on an uninhabited island and must learn to adapt to the harsh surroundings, gradually building relationships with the animals on the island and becoming the adoptive parent of an orphaned gosling."],
+];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            MovieTrails
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <!-- As a link -->
+         <div id="topnav" class="container-fluid">
+            <nav class="navbar navbar-expand-lg ">
+                <div class="topnav topnavlinks">
+                    <a class="logo navbar-brand" href="#">
+                        <img src="media/logo.png" alt="Logo" width="180" height="40">
+                    </a>
+                    <a class="topnavlinks navbar-brand" href="#">Home</a>
+                    <a class="topnavlinks navbar-brand" href="#featured">Movies</a>
+                    <a class="topnavlinks navbar-brand" href="#featured">Featured</a>
+                    <a class="topnavlinks navbar-brand" href="#featured">Favorites</a>
+                    <a class="topnavlinks navbar-brand" href="#" onclick="alert('Authors of the Webpage are: \n Peralta, Kenneth James V. \n Pidal, Rudolf Jake');">About</a>
+                </div>
+            </nav>
+         </div>
+       
+
+        <div class="bgvid">
+            <video autoplay muted loop>
+                <source src="media\movietrails.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+
+        <div id="featured">
+           <div class="featuredCont">Featured Movies</div>
+        </div>
+
+        <div class="container mt-4">
+            <div class="slide-in carousel-container position-relative" id="slide1">
+                <!-- Left Navigation Button -->
+                <button class="carousel-nav left" id="scrollLeft">
+                    &lt;
+                </button>
+
+                <div id="movieCarousel" class="movie-carousel">
+                    <?php
+                    foreach ($movies as $movie):
+                    ?>
+                        <div class="movie-item">
+                            <img src="<?php echo $movie['image']; ?>" class="d-block" alt="<?php echo $movie['title']; ?>" 
+                                data-bs-toggle="modal" data-bs-target="#trailerModal" 
+                                data-trailer="<?php echo $movie['trailer']; ?>" 
+                                data-title="<?php echo $movie['title']; ?>" 
+                                data-description="<?php echo $movie['description']; ?>" />
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+
+                <!-- Right Navigation Button -->
+                <button class="carousel-nav right" id="scrollRight">
+                    &gt;
+                </button>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div id="featured">
+           <div class="featuredCont">Popular Favorites</div>
+        </div>
+
+        <div class="container mt-4">
+            <div class="slide-in carousel-container position-relative" id="slide2">
+                <!-- Left Navigation Button -->
+                
+                <div id="movieCarousel" class="movie-carousel">
+                    <?php
+                    foreach ($movies2 as $movie):
+                    ?>
+                        <div class="movie-item">
+                            <img src="<?php echo $movie['image']; ?>" class="d-block" alt="<?php echo $movie['title']; ?>" 
+                                data-bs-toggle="modal" data-bs-target="#trailerModal" 
+                                data-trailer="<?php echo $movie['trailer']; ?>" 
+                                data-title="<?php echo $movie['title']; ?>" 
+                                data-description="<?php echo $movie['description']; ?>" />
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+
+                <!-- Right Navigation Button -->
+                
+            </div>
+        </div>
+
+        <!-- Trailer Modal with Description -->
+        <div class="modalwidthmax modal fade" id="trailerModal" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="movieTitle">Trailer</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body d-flex flex-column"> <!-- Change to vertical flex -->
+                        <!-- Video section -->
+                        <div class="video-container mb-3" style="text-align: center;">
+                            <video id="trailerVideo" controls style="width: 100%; height: auto;"></video>
+                        </div>
+
+                        <!-- Description at the bottom -->
+                        <div class="description-container" style="text-align: justify; padding-top: 450px;">
+                            <h4>Description</h4>
+                            <p id="movieDescription"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
+
